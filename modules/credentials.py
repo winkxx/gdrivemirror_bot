5e17e316567704d1c619129a60840941c8881f5f
winkxx <68945988+winkxx@users.noreply.github.com>
2021-01-06 22:51:24 +0800

    Update and rename credentials.py.example to credentials.py

---
+++ modules/credentials.py
@@ -0,0 +1,7 @@
+class Creds(object):
+    TOKEN = "1402684613:AAHY8G_qN8wRFHvT0d9NWfbweZRr8zrYT6I"
+    CLIENT_ID = "775399758105-4bk2esak2fihuomu90qj0f0gj3r1klcd.apps.googleusercontent.com"
+    CLIENT_SECRET = "o5yIzzgeYaCfTojnvzT7ZSGQ"
+    PATH_ID = "1mgsn6lJn2rypWlJcTKQt6qlqF_K6QhQ9"
+    USER_ID = "1367147811"
+
